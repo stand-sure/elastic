165de9a4d69e8dbbbb36e8162dc16cb3a70c6aaf
C. J. Anderson <chris@standsure.io>
2023-01-27 19:35:24 -0500

    wip: adding metricbeat, heartbeat

--- beats/install-beats.sh
+++ beats/install-beats.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-kubectl apply --filename quickstart-beats.yaml --namespace elastic-system
+kubectl apply --filename quickstart-beats-filebeat.yaml --namespace elastic-system
 
 read -p "hit enter to view beats" -r
 kubectl get beats --namespace elastic-system
